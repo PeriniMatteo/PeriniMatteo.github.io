2695238e967bde3d257a75285d77205120b11027
Matteo Perini <perini.matteo@gmail.com>
2015-10-13 11:27:42 +0300

    testphp2

---
+++ index.php
@@ -0,0 +1,14 @@
+
+<?php
+echo "Hello World\n";
+
+
+// You can use variables inside of an echo statement
+$foo = "foobar";
+$bar = "barbaz";
+
+echo "foo is $foo"; // foo is foobar
+$ciao = $_GET['ciao'];
+echo "$ciao"
+
+?>
